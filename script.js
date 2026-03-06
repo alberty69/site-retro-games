2067ca1e623f099ee6f904660f8a03dd24e37811
alberty69 <alberty.ramos@escola.pr.gov.br>
2026-03-06 15:11:01 -0300

    teste

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function gerarMensagem() {
-
-    let nome = document.getElementById("nomeUsuario").value;
-
-    if(nome === "") {
-        document.getElementById("resultado").innerText = 
-        "Por favor, digite seu nome!";
-    } else {
-
-        let mensagem = nome + 
-        ", relembre e jogue os jogos que você tanto amou";
-
-        document.getElementById("resultado").innerText = mensagem;
-    }
+      let nome = document.getElementById("nomeUsuario").value;
+        
+          if(nome === "") {
+              document.getElementById("resultado").innerText = "Por favor, insira o nome do Player 1!";
+                } else {
+                    let mensagem = nome + ", você tem potencial para zerar este fliperama!";
+                        document.getElementById("resultado").innerText = mensagem;
+                          }
+                          }
 }
